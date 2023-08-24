389ba2cf1d15ee33a020bf28862dafe4574b662e
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-08-24 16:04:57 +0700

    [fix] user.py

--- app/views/user.py
+++ app/views/user.py
@@ -35,7 +35,7 @@ def register_user (request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect(to='login')
+            return redirect(to='index')
 
     else:
         form = SignupForm()
