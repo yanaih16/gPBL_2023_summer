7ca7a7b3ab39ba464b06e65328adeb4c837875e1
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-08-24 12:28:06 +0700

    [add] user

--- match_market/settings.py
+++ match_market/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_bootstrap5',
     'app',
-    'members',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'app.User'
