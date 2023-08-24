dd250f5698a0502ebe1283f0fb3c64f6e24bfa28
GNLimerence <phamhoanghainam203@gmail.com>
2023-08-24 14:38:54 +0700

    [change] admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import User
+from .models import Item
+from .models import Tag
+from .models import Item_Tag
+from .models import Chat
+from .models import Matching
+from .models import review
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Item)
+admin.site.register(Tag)
+admin.site.register(Item_Tag)
+admin.site.register(Chat)
+admin.site.register(Matching)
+admin.site.register(review)
+
