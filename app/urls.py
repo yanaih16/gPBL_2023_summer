1a77d9fb60f8d3d928981bd6463a9255292704a9
pcmmi <pc.mmi331@gmail.com>
2023-08-27 09:32:44 +0700

    [change] add path (item_list)

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import top
-from .views import user
+from .views import user, item_list
 
 urlpatterns = [
     path("", top.index, name="index"),
     path("login", user.login_user, name = "login"),
     path("logout", user.logout_user, name = "logout"),
     path("register", user.register_user, name = "register"),
+    path("item_list", item_list.ItemList.as_view(), name="item_list"),
 ]
