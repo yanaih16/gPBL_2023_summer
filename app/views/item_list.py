12aca0bab9c01de55daa7708afbd8a16d62ea5ae
pcmmi <pc.mmi331@gmail.com>
2023-08-27 17:29:40 +0700

    [change] item_list.py

--- app/views/item_list.py
+++ app/views/item_list.py
@@ -1,8 +1,15 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from ..models import Item
 
-class ItemList(ListView):
+class ItemList(LoginRequiredMixin, ListView):
     model = Item
     context_object_name = "item_list"
-    template_name = "item_list/item_list.html"
+    template_name = "item/item_list.html"
+
+    # ログイン中のユーザーのみアクセス可能
+    def get_queryset(self):
+        return Item.objects.filter(user_id=self.request.user)
