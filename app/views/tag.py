f9b6efe004a306452cfc7593cf41439a8d3c284b
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-08-29 14:11:06 +0700

    [add] match

--- app/views/tag.py
+++ app/views/tag.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from app.models import Tag, Item
+from app.models import Tag, Item, User
+import json
+from django.http import JsonResponse
 
 @login_required
 def select_tags(request):
     if request.method == 'POST':
-        selected_tags = request.POST.getlist('tags')
-        items = Item.objects.filter(item_tag__tag_id__in=selected_tags).distinct()
-        return render(request, 'matching.html', {'items': items})
-    else:
-        tags = Tag.objects.all()
-        return render(request, 'select_tags.html', {'tags': tags})
+        data_list = Item.objects.all()
+        data = []
+        for item in data_list:
+            user = User.objects.get(id=item.user.id)
+            if request.user == user : 
+                continue
+            print(item.image.url)
+            data.append({
+                "id" : item.id,
+                "user" : user.username,
+                'name' : item.name, 
+                'text' : item.text, 
+                'image' : item.image.url,
+                'value' : item.value,
+            })
 
+        return render(request, 'tag/matching.html', {'items': json.dumps(data)})
+    else:
+        tag_list = Tag.objects.values("id", "name")
+        context = {
+            "title": "タグ設定",
+            "tag_list": tag_list,
+        }
+        return render(request, 'tag/select_tags.html', context)
