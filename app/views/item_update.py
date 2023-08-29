d068fa2cf0de27c6fe7c5a1f891a357730bc727d
pcmmi <pc.mmi331@gmail.com>
2023-08-29 12:17:37 +0700

    商品登録したユーザーしか編集できないようにした

--- app/views/item_update.py
+++ app/views/item_update.py
@@ -2,6 +2,8 @@ from django.views.generic import UpdateView
 from django.urls import reverse_lazy
 from ..models import Item
 from django import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class ItemForm(forms.ModelForm):
     class Meta:
@@ -9,8 +11,12 @@ class ItemForm(forms.ModelForm):
         fields = ['name', 'text', 'value', 'image']  # 必要なフィールドのみを指定
 
 
-class ItemUpdateView(UpdateView):
+class ItemUpdateView(LoginRequiredMixin, UpdateView):
     model = Item
     form_class = ItemForm
     template_name = 'item/item_update.html'
     success_url = reverse_lazy("item_list")  # 成功時のリダイレクト先を指定します。
+
+    # ログイン中のユーザーのみアクセス可能
+    def get_queryset(self):
+        return Item.objects.filter(user_id=self.request.user)
