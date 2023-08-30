04a1cef7b3400ee80a91ada5983be5684a3ad9bb
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-08-30 09:33:06 +0700

    [update] matching

--- app/views/tag.py
+++ app/views/tag.py
@@ -5,19 +5,17 @@ import json
 from gensim.models import KeyedVectors
 import operator
 
-# model_dir = "./word2vec/entity_vector.model.bin"
-# model = KeyedVectors.load_word2vec_format(model_dir, binary=True)
+model_dir = "./word2vec/entity_vector.model.bin"
+model = KeyedVectors.load_word2vec_format(model_dir, binary=True)
 
 @login_required
 def select_tags(request):
     if request.method == 'POST':
-        print(request.POST.getlist('tags'))
         tag_id_list = request.POST.getlist('tags')
         post_tags = []
         for tag_id in tag_id_list:
             tag = Tag.objects.get(id=tag_id)
             post_tags.append(tag.name)
-        print(post_tags)
         data_list = Item.objects.all()
         data = []
         for item in data_list:
@@ -27,16 +25,16 @@ def select_tags(request):
             item_tags = Item_Tag.objects.filter(item=item.id)
             count = 0
             per = 0.0
-            # for item_tag in item_tags:
-            #     tag = Tag.objects.get(id=item_tag.tag.id)
-            #     for post_tag in post_tags:
-            #         print(tag.name +" "+post_tag)
-            #         per += model.similarity(tag.name, post_tag)
-            #         count += 1
-            # per /= count
-            print(per)
+            for item_tag in item_tags:
+                tag = Tag.objects.get(id=item_tag.tag.id)
+                for post_tag in post_tags:
+                    print(tag.name +" "+post_tag)
+                    per += model.similarity(tag.name, post_tag)
+                    count += 1
+            per /= count
             data.append({
-                "id" : item.id,
+                "item_id" : item.id,
+                "user_id" : user.id,
                 "user" : user.username,
                 'name' : item.name, 
                 'text' : item.text, 
@@ -44,8 +42,8 @@ def select_tags(request):
                 'value' : item.value,
                 'per' : per,
             })
-        data = print(sorted(data, key=operator.itemgetter('per')))
-        return render(request, 'tag/matching.html', {'items': json.dumps(data)})
+        a = sorted(data, key=operator.itemgetter('per'), reverse=True)
+        return render(request, 'tag/matching.html', {'items': json.dumps(a)})
     else:
         tag_list = Tag.objects.values("id", "name")
         context = {
@@ -53,3 +51,4 @@ def select_tags(request):
             "tag_list": tag_list,
         }
         return render(request, 'tag/select_tags.html', context)
+
