655001aecd3f610b676ad9bebf18db82eb7a84f4
mchau98 <nguynminhchau98@gmail.com>
2023-08-31 09:26:16 +0700

    [update] review

--- app/views/user_review.py
+++ app/views/user_review.py
@@ -1,40 +1,24 @@
-from django.shortcuts import get_object_or_404
-from ..models import Review, Matching, User
-from ..serializers import ReviewSerializer 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from django.shortcuts import render, redirect
+from django.db.models import Avg
+from ..models import Review, Matching
+from ..forms.review import ReviewForm
 
-@api_view(['POST'])
-def add_review(request):
-    rater_id = request.data.get('rater')
-    evaluator_id = request.data.get('evaluator')
-    score = int(request.data.get('score', 0))
-    text = request.data.get('text', '')
+def rate_seller(request, matching_id):
+    matching = Matching.objects.get(id=matching_id)
+    seller = matching.seller
+    buyer = matching.buyer
 
-    if rater_id is None or evaluator_id is None or score < 1 or score > 5:
-        return Response({'error': 'Invalid data.'}, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'POST':
+        score = int(request.POST['rater'])
+        text = request.POST['text']
 
-    try:
-        rater = User.objects.get(pk=rater_id)
-        evaluator = User.objects.get(pk=evaluator_id)
-    except User.DoesNotExist:
-        return Response({'error': 'Invalid user ID.'}, status=status.HTTP_400_BAD_REQUEST)
+        review = Review.objects.create(rater=matching, evaluator=matching, score=score, text=text)
+        review.save()
+        # Calculate average rating for the seller
+        average_rating = Review.objects.filter(evaluator=matching).aggregate(Avg('score'))['score__avg']
+        seller.average_rating = average_rating
+        seller.save()
 
-    review = Review.objects.create(rater=rater, evaluator=evaluator, score=score, text=text)
-    serializer = ReviewSerializer(review)
-    
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return redirect('match')
 
-
-@api_view(['GET'])
-def get_reviews(request, user_id):
-    try:
-        user = User.objects.get(pk=user_id)
-    except User.DoesNotExist:
-        return Response({'error': 'Invalid user ID.'}, status=status.HTTP_400_BAD_REQUEST)
-
-    reviews_received = Review.objects.filter(evaluator=user)
-    serializer = ReviewSerializer(reviews_received, many=True)
-
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    return render(request, 'review/review.html', {'matching': matching})
